cb38f44e2da7fabad0890967c4d3255e5a109fb1
Wolfgang Kreminger <r4pt0s@protonmail.com>
2023-02-19 17:32:26 +0200

    add Main.java din CtrlN-code

---
+++ CtrlN-code/PrimaAplicatieJava/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //JAR - Java Archive
+        //JAVA-HOME
+        System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
