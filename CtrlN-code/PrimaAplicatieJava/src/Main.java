689812c6c28e547e2e2277d8def0dc4ca9809f20
Wolfgang Kreminger <r4pt0s@protonmail.com>
2023-02-19 19:07:29 +0200

    modificare java

--- CtrlN-code/PrimaAplicatieJava/src/Main.java
+++ CtrlN-code/PrimaAplicatieJava/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        //JAR - Java Archive
+
         //JAVA-HOME
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
