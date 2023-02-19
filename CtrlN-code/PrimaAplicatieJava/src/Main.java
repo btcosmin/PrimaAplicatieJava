32ae1b3c53a43d195f2f6ef607b6112e6f225153
Wolfgang Kreminger <r4pt0s@protonmail.com>
2023-02-19 20:46:05 +0200

    Am adugat java doc

--- CtrlN-code/PrimaAplicatieJava/src/Main.java
+++ CtrlN-code/PrimaAplicatieJava/src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
-    //aceasta este metoda main, care porneste o aplicatie Java
-    public static void main(/* args este parametrul metodei main*/String[] args) {
-        /*
-        Inainte de comentariu nu se poate pune text
-        aici vrem sa afisam pe consola Intelij un mesaj ales de noi
-        Textul de aici face parte tot din comentariul multi-linie
-         */
+    /**
+     * Metoda main este apelata de masina virtuala Java si care porneste  o aplicatie Java
+     * Putem sa transmitem argumente aditionale prin parametrul <b>args</b>
+     * @param args Acesta este parametrul cu care transmitem argumente programului nostru
+     * @author Blotu Cosmin
+     */
+    public static void main(String[] args) {
+
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
